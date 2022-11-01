96b57d3de7896548d855591bbed21bdd7b64838c
SoNeonGO <110576417+SoNeonGO@users.noreply.github.com>
2022-11-01 18:10:25 -0400

    Add files via upload

---
+++ try_glut/try_glut/ground.hpp
@@ -0,0 +1,24 @@
+//
+//  ground.hpp
+//  try_glut
+//
+//  Created by Qianhuang Li on 11/1/22.
+//
+
+#ifndef ground_hpp
+#define ground_hpp
+
+#define GL_SILENCE_DEPRECATION
+#include <stdio.h>
+#pragma once
+ 
+#include <GLUT/GLUT.h>
+#include <GLFW/glfw3.h>
+ 
+class Ground {
+public:
+    static void Draw();
+};
+
+
+#endif /* ground_hpp */
