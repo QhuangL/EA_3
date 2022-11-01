542477433da17ff7cfd6c13326e78ca35126033c
SoNeonGO <110576417+SoNeonGO@users.noreply.github.com>
2022-11-01 01:44:04 -0400

    camera+polygon

--- sim_try/sim_try/Visualize.cpp
+++ sim_try/sim_try/Visualize.cpp
@@ -81,7 +81,7 @@ void Visualizer::input(float* vertices, int sizeofV, unsigned int* indices, int 
     // glBindVertexArray(0);
 
     // uncomment this call to draw in wireframe polygons.
-    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+//    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 }
 
 void processInput(GLFWwindow *window){
@@ -105,16 +105,16 @@ void Visualizer::inloop1(){
         this->corShader->use();
 
     // create camera
-        glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 1.0f);
-        glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+        glm::vec3 cameraPos = glm::vec3(0.0f, 100.0f, -30.0f);
+        glm::vec3 cameraTarget = glm::vec3(5.0f, 5.0f, 5.0f);
         glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
         glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
         glm::vec3 cameraRight = glm::normalize(glm::cross(up, cameraDirection));
         glm::vec3 cameraUp = glm::cross(cameraDirection, cameraRight);
         glm::mat4 viewc;
-        viewc = glm::lookAt(glm::vec3(0.0f, 100.0f, -20.0f),
-               glm::vec3(0.0f, 0.0f, 0.0f),
-               glm::vec3(0.0f, 1.0f, 0.0f));
+        viewc = glm::lookAt(cameraPos,
+               cameraTarget,
+                up);
         
     
     
