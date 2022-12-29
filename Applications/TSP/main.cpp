4081cf2473cc6783a917eed65ab79893505cb94f
hanzhuchen666 <68385644+hanzhuchen666@users.noreply.github.com>
2022-12-28 21:06:42 -0500

    rebuild finished, cv2 loaded and showed image successfully

--- Applications/TSP/main.cpp
+++ Applications/TSP/main.cpp
@@ -4,8 +4,8 @@
 int main(int, char**) {
     std::cout << "Hello, world!\n";
     cv::Mat img = cv::imread("C:\\Users\\Shawn\\Documents\\GitHub\\EA_3\\Applications\\TSP\\data\\cakerecipte.png",0);
-    // cv::imshow("img",img);
+    cv::imshow("img",img);
     
-    // cv::waitKey(0);
+    cv::waitKey(0);
     return 0;
 }
